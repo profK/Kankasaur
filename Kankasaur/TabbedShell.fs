4de7bbcf208b196a878d9ee3cd5e27af5d464c99
professorK <jeffpk@gmail.com>
2025-04-02 23:53:16 -0400

    wrote TabbedShell init

--- Kankasaur/TabbedShell.fs
+++ Kankasaur/TabbedShell.fs
@@ -9,9 +9,11 @@ type ShellMag =
 type PluginRecord = {
      Name : string
      Instance : Kankasaur.PluginInterface.IPlugin
- }   
+     State: Kankasaur.PluginInterface.IPluginState
+ }
+  
     
-type ShellState = { plugins : Kankasaur.PluginInterface.IPluginState list }
+type ShellState = { plugins : PluginRecord list }
  
 let mutable plugins = []
  
@@ -19,11 +21,19 @@ let init() =
      AppDomain.CurrentDomain().GetAssemblies()
      |> Array.iter ManagerRegistry.scanAssembly
      
-     plugins <-
-         (ManagerRegistry.getAllManagers<PluginInterface.IPlugin>()
-         |> List.map (fun Iplugin ->
+     ManagerRegistry.getAllManagers<PluginInterface.IPlugin>()
+     |> List.map (fun Iplugin ->
             Iplugin.GetType().
                     GetCustomAttribute<ManagerRegistry.Manager>()
-            |> fun attr -> {Name = attr.Name; Instance = Iplugin}))       
+            |> fun attr -> {
+                                        Name = attr.Name
+                                        Instance = Iplugin
+                                        State = Iplugin.Init()
+                                    }
+         )
+     |> fun pluginRecs -> {plugins = pluginRecs} 
+     
+
+    
          
 
