2b6b5c774f6dd1354a615298b371cf9ecf8283aa
professorK <jeffpk@gmail.com>
2025-04-04 13:39:20 -0400

    counter plugin compiles

--- Kankasaur/Counter.fs
+++ Kankasaur/Counter.fs
@@ -1,24 +1,27 @@
 ﻿namespace Kankasaur
 
 open Avalonia.FuncUI.Types
+open Kankasaur.PluginInterface
+open ManagerRegistry
 
 module Counter =
     open Avalonia.Controls
     open Avalonia.FuncUI.DSL
     open Avalonia.Layout
     
-    type State = { count : int }
+    type CounterState = { count : int } interface IPluginState
     let init = { count = 0 }
 
-    type CounterMsg = Increment | Decrement | Reset
+    type CounterMsg = Increment | Decrement | Reset  interface IPluginMsg
 
-    let update (msg: CounterMsg) (state: State) : State =
+
+    let update (msg: CounterMsg) (state: CounterState) :CounterState =
         match msg with
         | Increment -> { state with count = state.count + 1 }
         | Decrement -> { state with count = state.count - 1 }
         | Reset -> init
     
-    let view (state: State) (dispatch: obj -> unit   ) : IView=
+    let view (state: CounterState) (dispatch: obj -> unit   ) : IView=
         DockPanel.create [
             DockPanel.children [
                 Button.create [
@@ -45,3 +48,15 @@ module Counter =
                 ]
             ]
         ]
+    [<ManagerRegistry.Manager("Counter",
+           supportedSystems.Linux|||supportedSystems.Windows|||supportedSystems.Mac)>]
+    type CounterPlugin() =
+        interface IPlugin with
+            member this.Init() = init :> IPluginState
+            member this.Update(msg:IPluginMsg) (state:IPluginState) =
+                let msg = msg :?> CounterMsg
+                update msg (state :?> CounterState) :> IPluginState
+          
+            member this.View(state:IPluginState) (dispatch:obj -> unit) =
+                view (state :?> CounterState) dispatch :> IView
+                    
